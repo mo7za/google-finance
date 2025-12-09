00ee35f0174b9e10cf9e367c242dcda42e1441d4
mohamed osman <moh.osman123@hotmail.com>
2025-12-09 19:42:39 +0200

    enhancing the code structure

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-import requests as r
+import requests as r 
 from bs4 import BeautifulSoup
 
-
 def get_price_information(ticker, exchange):
-
-    url = f"https://www.google.com/finance/qoute/{ticker}:{exchange}"
+    url = f"https://www.google.com/finance/quote/{ticker}:{exchange}"
     resp = r.get(url) 
-    soup =BeautifulSoup(resp.content, "html.parser")
+    soup = BeautifulSoup(resp.content, "html.parser")
 
-    price_div = soup.find("div",attrs={"data_last_price": true})
-    #  price = float(price_div {"data-last-price"})
-   
-    return {
-        "ticker" : ticker,
+    price_div = soup.find("div", attrs={"data-last-price": True})
+    price = float(price_div["data-last-price"])
+    currency = price_div["data-currency-code"]
+
+    return  {
+        "ticker": ticker,
         "exchange": exchange,
-        "price":price,
-        "currency":currency
-    }
-print("hey")
+        "price": price,
+        "currency": currency
+    }   
+
+
 if __name__ == "__main__":
-    print("hello")
-    print(get_price_information("MSFT", "NASDAQ"))
-    print("hi")    
+     print(get_price_information("MSFT", "NASDAQ"))
